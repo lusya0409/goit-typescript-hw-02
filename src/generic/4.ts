11e7cce9082286dd554146382b19d254781d6e8f
lusya0409 <lusya0409@gmail.com>
2023-10-30 22:18:05 +0200

    chandes generic ts 1-8

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -1,15 +1,16 @@
 /*
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
-
-class Component {
-  constructor (public props:T) {
-
-  }
+interface Titable {
+  title: string;
 }
 
-class Page extends Component {
-  pageInfo () {
+class Component<T> {
+  constructor(public props: T) {}
+}
+
+class Page extends Component<Titable> {
+  pageInfo() {
     console.log(this.props.title);
   }
 }
